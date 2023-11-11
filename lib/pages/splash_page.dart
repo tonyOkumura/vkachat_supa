a76069dfa9ffefcadc3a6ce029168db81052bcd4
Tokumura <anton.tereshchenko.0003@bk.ru>
2023-11-11 18:52:59 +0300

    add animation splash

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:vkachat_supa/pages/chat_page.dart';
 import 'package:vkachat_supa/pages/home_page.dart';
 import 'package:vkachat_supa/pages/register_page.dart';
 import 'package:vkachat_supa/utils/constants.dart';
 
-/// Page to redirect users to the appropriate page depending on the initial auth state
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
@@ -12,29 +10,137 @@ class SplashPage extends StatefulWidget {
   SplashPageState createState() => SplashPageState();
 }
 
-class SplashPageState extends State<SplashPage> {
+class SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _iconScaleAnimation;
+  late Animation<double> _textOpacityAnimation;
+  late Animation<Offset> _textSlideAnimation;
+
   @override
   void initState() {
     super.initState();
+
+    _animationController = AnimationController(
+      duration: Duration(seconds: 4),
+      vsync: this,
+    );
+
+    _iconScaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.0, 0.6, curve: Curves.easeInOutQuad),
+      ),
+    );
+
+    _textOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.6, 1.0, curve: Curves.easeInOutQuad),
+      ),
+    );
+
+    _textSlideAnimation = Tween<Offset>(
+      begin: Offset(0.0, 0.5),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.6, 1.0, curve: Curves.easeInOutQuad),
+      ),
+    );
+
+    _startAnimations();
+  }
+
+  Future<void> _startAnimations() async {
+    await _animationController.forward(); // Start icon animation
+    await Future.delayed(Duration(milliseconds: 500)); // Optional delay
+    await _animationController.forward();
+    // Start text animation
+    await Future.delayed(
+        Duration(milliseconds: 500)); // Delay before redirection
     _redirect();
   }
 
   Future<void> _redirect() async {
-    // await for for the widget to mount
-    await Future.delayed(Duration.zero);
-
     final session = supabase.auth.currentSession;
     if (session == null) {
-      Navigator.of(context)
-          .pushAndRemoveUntil(RegisterPage.route(), (route) => false);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RegisterPage(
+            isRegistering: false,
+          ),
+        ),
+      );
     } else {
-      Navigator.of(context)
-          .pushAndRemoveUntil(HomePage.route(), (route) => false);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: preloader);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _iconScaleAnimation.value,
+                  child: Icon(
+                    Icons.message,
+                    size: 150,
+                    color: Colors.blue,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            SlideTransition(
+              position: _textSlideAnimation,
+              child: FadeTransition(
+                opacity: _textOpacityAnimation,
+                child: Text(
+                  'VKA CHAT',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            SlideTransition(
+              position: _textSlideAnimation,
+              child: FadeTransition(
+                opacity: _textOpacityAnimation,
+                child: Text(
+                  'Простой, Защищенный, Свой',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 24,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
